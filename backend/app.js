9aab92ba453dec5d3bceead7fa9751da89dab869
152334H <54623771+152334H@users.noreply.github.com>
2022-12-08 08:25:47 +0800

    add express

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import predict from './gpt.js'
+
+const app = express()
+
+app.use(express.json())
+app.use((req, _, nxt) => {
+	console.log(new Date().toUTCString())
+	console.log(req.method)
+	console.log(req.path)
+	console.log(req.body)
+	console.log('---')
+	nxt()
+})
+
+app.post('/_test', async (req, res) => {
+    const query = req.body.query
+    if (typeof query !== 'string')
+        return res.status(400).send({
+            error: 'query not provided'
+        })
+   
+    const ls = await predict(query)
+    if (ls[0] === 0) {
+        res.status(200).send({
+            variant: 'help'
+        })
+    } else if (ls[0] === 1) {
+        res.status(200).send({
+            variant: 'item',
+            item: ls[1]
+        })
+    } else {
+        res.status(400).send({
+            error: `Received garbage: ${ls[1]}`
+        })
+    }
+})
+app.get('/', (_, res) => {
+  res.send('Hello World')
+})
+
+export default app;
--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,6 @@
-// this just exists for testing right now!!!
+import app from './app.js'
 
-import predict from './gpt.js'
-
-import readline from "readline";
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
-process.stdout.write('> ')
-rl.on('line', (line) => {
-    predict(line).then(ls => {
-        if (ls[0] === 0) {
-            console.log("Got a help request")
-        } else if (ls[0] === 1) {
-            console.log(`User searched for a: "${ls[1]}"`)
-        } else {
-            console.error("Got bullshit:", ls)
-        }
-    }).then(() => process.stdout.write('> '))
-});
-
-rl.once('close', () => {
-     // end of input
- });
-
+const PORT = 5170
+app.listen(PORT, () => {
+    console.log(`Backend running on ${PORT}`)
+})
